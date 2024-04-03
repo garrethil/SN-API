105b27f0602b472612ff6e34fcad9e6b45d2bb3e
garrethil <garrethildebrandt@gmail.com>
2024-04-03 15:36:09 -0400

    complete models

--- models/thought.js
+++ models/thought.js
@@ -1,26 +1,53 @@
 const { Schema, model } = require('mongoose');
 
+
+const reactionSchema = new Schema({
+   reactionId: {
+    type: Schema.Types.ObjectId,
+    default: mongoose.Types.ObjectId
+    
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactedAt: {
+        type: Date,
+        default: Date.now,
+        get: function(date) {
+            return date.toLocaleString();
+        }
+    }
+});
+
+
 const thoughtSchema = new Schema(
     {
         thoughtText: {
-            Type: String,
+            type: String,
             required: true,
             maxlength: 280
         },
         createdAt: {
             type: Date,
-
+            default: Date.now,
+            get: function(date) {
+                return date.toLocaleString();
+            }
         },
         username: {
-            Type: String,
+            type: String,
             required: true
         },
-        reactions: [{
-            
-        }],
+        reactions: [reactionSchema],
     }
 );
 
-const Thought = model('thought', thoughtSchema);
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
