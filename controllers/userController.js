d27cfd527295a159c06cffa04f90f6a032ad4c71
garrethil <garrethildebrandt@gmail.com>
2024-04-03 18:09:12 -0400

    add update and delete methods to User controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,7 @@ module.exports = {
     // Get a User
     async getSingleUser(req, res) {
         try {
-            const user = await User.findOne({ _id: req.params.id });
+            const user = await User.findOne({ _id: req.params.userId });
             if (!user) {
                 return res.status(404).json({ message: 'User not found' });
             }
@@ -33,6 +33,37 @@ module.exports = {
         } catch(err) {
             console.error(err);
             res.status(500).json({ message: 'Internal server error' });
+        }
+    },
+
+    // Update a User
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                {_id: req.params.userId},
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+            res.json(user);
+        } catch(err) {
+            console.error(err);
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    },
+
+    // Delete a User
+
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndRemove({ _id: req.params.userId })
+
+            if (!user) {
+                return res.status(404).json({ message: 'No such User exists' });
+            }
+
+            res.json({ message: 'User successfully deleted' });
+        } catch(err) {
+            res.status(500).json(err);
         }
     }
 }
